0cfa407e4be9563bd276b3999cc8d725aee36bd5
childressdev <jcampbell@childressagency.com>
2018-10-16 10:36:03 -0400

    added header class

--- page.php
+++ page.php
@@ -1,15 +1,15 @@
 <?php get_header(); ?>
-  <main id="main" class="hp-main">
+  <main id="main">
     <div class="container">
       <article>
-        <header>
+        <header class="page-header">
           <?php if(get_field('page_title')): ?>
             <h2 class="page-title"><?php the_field('page_title'); ?></h2>
           <?php endif; if(get_field('page_subtitle')): ?>
             <h3 class="page-subtitle"><?php the_field('page_subtitle'); ?></h3>
           <?php endif; ?>
         </header>
-        
+
         <?php if(have_posts()): while(have_posts()): the_post(); ?>
           <?php the_content(); ?>
         <?php endwhile; endif; ?>
