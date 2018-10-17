4d489e1fb93be921a0b6591022bec6c4bb3285ca
childressdev <jcampbell@childressagency.com>
2018-10-17 14:58:00 -0400

    added

--- single-products.php
+++ single-products.php
@@ -1,275 +1,73 @@
 <?php get_header(); ?>
   <main id="main" class="hp-main">
     <div class="container">
-      <article>
-        <header>
-          <h2 class="article-title">Vegetable Seed</h2>
-          <h3 class="article-subtitle">Vegetable Seed in bulk.<br />Buying in bulk saves lots of money!</h3>
-          <p>Roxbury carries the following seed in bulk. These seeds are all HEIRLOOM seeds with the exception of a few varieties of the newer hybrid sweet corns. Many of the older hybrid sweet corns are still considered heirloom seeds.</p>
-          <p>Our bulk seeds are not certified as being organically grown. NONE OF THESE SEEDS ARE GMO!</p>
-          <p>They come with seasoned advice as we weigh them out right before your very eyes.</p>
-        </header>
-      <section id="product-tables">
-        <ul class="nav nav-justified" role="tablist">
-          <li role="presentation" class="active">
-            <a href="#large-seed" aria-controls="large-seed" role="tab" data-toggle="tab">Large Seed</a>
-          </li>
-          <li role="presentation">
-            <a href="#small-seed" aria-controls="small-seed" role="tab" data-toggle="tab">Small Seed</a>
-          </li>
-          <li role="presentation">
-            <a href="#vegetable-roots" aria-controls="vegetable-roots" role="tab" data-toggle="tab">Vegetable Roots</a>
-          </li>
-        </ul>
-        <div class="tab-content">
-          <div id="large-seed" class="tab-pane fade in active" role="tabpanel">
-            <!-- regex to add $ to dollar values but ignore others: preg_replace('/\d+\.\d{2}/', '$$0', $str) -->
-            <div class="responsive-table">
-              <table class="table table-condensed table-striped">
-                <thead>
-                  <tr>
-                    <th>Large Seed for Vegetables</th>
-                    <th>Per Pound</th>
-                    <th>1/2 Pound</th>
-                    <th>1/4 Pound</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">Bush Beans - Blue Lake #274</th>
-                    <td>$3.50</td>
-                    <td>$2.20</td>
-                    <td>$1.40</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Bush Beans - Contender</th>
-                    <td>$3.00</td>
-                    <td>$1.90</td>
-                    <td>$1.20</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Bush Beans - KY Wonder</th>
-                    <td>$3.80</td>
-                    <td>$2.40</td>
-                    <td>$1.50</td>
-                  </tr>
-                </tbody>
-                <tbody>
-                  <tr>
-                    <th scope="row">Bush Lima - Burpee Improved</th>
-                    <td>$3.95</td>
-                    <td>$2.50</td>
-                    <td>$1.50</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Bush Lima - Cangreen / Thorogreen</th>
-                    <td>$3.95</td>
-                    <td>$2.50</td>
-                    <td>$1.50</td>
-                  </tr>
-                </tbody>
-                <tbody>
-                  <tr>
-                    <th scope="row">Bush Peas - Early Alaska</th>
-                    <td>$3.60</td>
-                    <td>$2.25</td>
-                    <td>$1.40</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Bush Peas - Early Frosty</th>
-                    <td>Not available</td>
-                    <td>in 2014</td>
-                    <td>&nbsp;</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Bush -Peas - Cascadia</th>
-                    <td>$2.95</td>
-                    <td>$1.80</td>
-                    <td>$1.20</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+      <?php get_template_part('partials/page-header'); ?>
+
+      <?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile; endif; ?>
+
+      <?php if(have_rows('table_category')): ?>
+        <section id="product-tables">
+          <ul class="nav nav-justified" role="tablist">
+            <?php $c=0; while(have_rows('table_category')): the_row(); ?>
+              <li role="presentation"<?php if($c==0){ echo ' class="active"'; } ?>>
+                <a href="#<?php echo sanitize_title(get_sub_field('table_category_title')); ?>" aria-controls="<?php echo sanitize_title(get_sub_field('table_category_title')); ?>" role="tab" data-toggle="tab"><?php the_sub_field('table_category_title'); ?></a>
+              </li>
+            <?php $c++; endwhile; ?>
+          </ul>
+
+          <div class="tab-content">
+            <?php $t=0; while(have_rows('table_category')): the_row(); ?>
+              <div id="<?php echo sanitize_title(get_sub_field('table_category_title')); ?>" class="tab-pane fade<?php if($t==0){ echo ' in active'; } ?>" role="tabpanel">
+                <!-- regex to add $ to dollar values but ignore others: preg_replace('/\d+\.\d{2}/', '$$0', $str) -->
+                <div class="responsive-table">
+                  <table class="table table-condensed table-striped">
+                    <?php $table_column_headers = []; ?>
+                    <?php if(have_rows('table_column_headers')): ?>
+                      <thead>
+                        <tr>
+                          <?php while(have_rows('table_column_headers')): the_row(); ?>
+                            <th><?php the_sub_field('table_column_header'); ?></th>
+                          <?php endwhile; ?>
+                        </tr>
+                      </thead>
+                      <?php $table_column_headers = get_sub_field('table_column_headers'); ?>
+                    <?php endif; ?>
+
+                    <?php if(have_rows('table_section')): while(have_rows('table_section')): the_row(); ?>
+                      <tbody>
+                        <?php if(have_rows('table_section_row')): while(have_rows('table_section_row')): the_row(); ?>
+                          <tr>
+                            <th scope="row"><?php the_sub_field('row_title'); ?></th>
+                            <?php
+                              $column_count = count($table_column_headers);
+                              $row_fields = get_field('row_fields');
+
+                              if(!empty($row_fields)){
+                                for($f = 0; $f < $column_count - 1; $f++){
+                                  $row_value = $row_fields[$f]['row_field'] ? $row_fields[$f]['row_field'] : '&nbsp;';
+                                  echo '<td>' . preg_replace('/\d+\.\d{2}/', '$$0', $row_value) . '</td>';
+                                }
+                              }
+                            ?>
+                          </tr>
+                        <?php endwhile; endif; ?>
+                      </tbody>
+                    <?php endwhile; endif; ?>
+                  </table>
+                </div>
+              </div>
+            <?php $t++; endwhile; ?>
           </div>
 
-          <div id="small-seed" class="tab-pane fade" role="tabpanel">
-            <div class="table-responsive">
-              <table class="table table-condensed">
-                <thead>
-                  <tr>
-                    <th>Small Seed for Vegetables</th>
-                    <th>Per Ounce</th>
-                    <th>1/2 Ounce</th>
-                    <th>1/4 Ounce</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <table class="table table-striped">
-                        <tbody>
-                          <tr>
-                            <th scope="row">Bush Beans - Blue Lake #274</th>
-                            <td>$3.50</td>
-                            <td>$2.20</td>
-                            <td>$1.40</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Beans - Contender</th>
-                            <td>$3.00</td>
-                            <td>$1.90</td>
-                            <td>$1.20</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Beans - KY Wonder</th>
-                            <td>$3.80</td>
-                            <td>$2.40</td>
-                            <td>$1.50</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      <table class="table table-striped">
-                        <tbody>
-                          <tr>
-                            <th scope="row">Bush Lima - Burpee Improved</th>
-                            <td>$3.95</td>
-                            <td>$2.50</td>
-                            <td>$1.50</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Lima - Cangreen / Thorogreen</th>
-                            <td>$3.95</td>
-                            <td>$2.50</td>
-                            <td>$1.50</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Lima - Dixie Butterpea</th>
-                            <td>$3.95</td>
-                            <td>$2.50</td>
-                            <td>$1.50</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      <table class="table table-striped">
-                        <tbody>
-                          <tr>
-                            <th scope="row">Bush Pease - Early Alaska</th>
-                            <td>$3.60</td>
-                            <td>$2.25</td>
-                            <td>$1.40</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Pease - Early Frosty</th>
-                            <td>Not available</td>
-                            <td>in 2014</td>
-                            <td>&nbsp;</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush -Pease - Cascadia</th>
-                            <td>$2.95</td>
-                            <td>$1.80</td>
-                            <td>$1.20</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </td>
-                  </tr>
-                </tbody>                
-              </table>
-            </div>
-          </div>
-
-          <div id="vegetable-roots" class="tab-pane fade" role="tabpanel">
-            <div class="table-responsive">
-              <table class="table table-condensed">
-                <thead>
-                  <tr>
-                    <th>Vegetable Roots, Tubers & Bulbs</th>
-                    <th>Per Pound</th>
-                    <th>1/2 Pound</th>
-                    <th>1/4 Pound</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <table class="table table-striped">
-                        <tbody>
-                          <tr>
-                            <th scope="row">Bush Beans - Blue Lake #274</th>
-                            <td>$3.50</td>
-                            <td>$2.20</td>
-                            <td>$1.40</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Beans - Contender</th>
-                            <td>$3.00</td>
-                            <td>$1.90</td>
-                            <td>$1.20</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Beans - KY Wonder</th>
-                            <td>$3.80</td>
-                            <td>$2.40</td>
-                            <td>$1.50</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      <table class="table table-striped">
-                        <tbody>
-                          <tr>
-                            <th scope="row">Bush Lima - Burpee Improved</th>
-                            <td>$3.95</td>
-                            <td>$2.50</td>
-                            <td>$1.50</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Lima - Cangreen / Thorogreen</th>
-                            <td>$3.95</td>
-                            <td>$2.50</td>
-                            <td>$1.50</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Lima - Dixie Butterpea</th>
-                            <td>$3.95</td>
-                            <td>$2.50</td>
-                            <td>$1.50</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      <table class="table table-striped">
-                        <tbody>
-                          <tr>
-                            <th scope="row">Bush Pease - Early Alaska</th>
-                            <td>$3.60</td>
-                            <td>$2.25</td>
-                            <td>$1.40</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush Pease - Early Frosty</th>
-                            <td>Not available</td>
-                            <td>in 2014</td>
-                            <td>&nbsp;</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">Bush -Pease - Cascadia</th>
-                            <td>$2.95</td>
-                            <td>$1.80</td>
-                            <td>$1.20</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
         </div>
       </section>
-      <div class="products-disclaimer">
-        <p>All prices and availability are subject to change. Please excuse any errors. We do the best we can.</p>
-      </div>
+      <?php if(get_field('price_disclaimer')): ?>
+        <div class="products-disclaimer">
+          <p><?php the_field('price_disclaimer'); ?></p>
+        </div>
+      <?php endif; ?>
     </div>
     <div class="bottom-dirt"></div>
   </main>
